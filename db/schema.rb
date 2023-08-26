0123f9739e523248bc833f42d1f8874a3cda63c1
shuto kikai <yysskikai04@icloud.com>
2023-08-26 23:21:30 +0900

    マイグレーションファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_23_045204) do
+ActiveRecord::Schema.define(version: 2023_08_26_141802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2023_08_23_045204) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.date "expired_at", default: -> { "CURRENT_DATE" }, null: false
+    t.string "status", default: "未着手", null: false
   end
 
   create_table "users", force: :cascade do |t|
