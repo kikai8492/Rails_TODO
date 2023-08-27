ac64a3dae26242495e7e60467d2b01f4a320232d
shuto kikai <yysskikai04@icloud.com>
2023-08-27 10:12:00 +0900

    index画面に優先度が表示されなかったのでその修正とshaw画面にタスクが出るようにした

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   validates :not_started_yet, presence: true
   validates :content, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_26_141802) do
+ActiveRecord::Schema.define(version: 2023_08_27_010001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2023_08_26_141802) do
     t.integer "user_id"
     t.date "expired_at", default: -> { "CURRENT_DATE" }, null: false
     t.string "status", default: "未着手", null: false
+    t.integer "priority", default: 0, null: false
   end
 
   create_table "users", force: :cascade do |t|
